4a1b5e9fd63060edab0e43903134397ddd4f0c88
c0dex <petrovgm@gmail.com>
2016-03-15 12:10:48 +0300

    Added bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../src/bootstrap.php';
+
+# Identification tokens for client's API calls. Example from web site - surely is expired. Use your own one
+define('API_CLIENT_TOKEN', '');
+define('API_USERID_TOKEN', '');
+
