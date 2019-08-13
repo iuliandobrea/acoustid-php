dd5f098a414214c5c61cef4df3ffc3dccbf5f5ec
c0dex <petrovgm@gmail.com>
2019-08-14 00:37:44 +0300

    Tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,5 @@
 # Identification tokens for client's API calls. Example from web site - surely is expired. Use your own one
 $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
 $dotenv->load();
+
+$dotenv->required(['API_APPLICATION_TOKEN', 'API_USER_TOKEN']);
