e9460db3c2ec7996abced64846a333754e54943c
c0dex <petrovgm@gmail.com>
2019-08-14 00:18:18 +0300

    Tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../src/bootstrap.php';
-
 # Identification tokens for client's API calls. Example from web site - surely is expired. Use your own one
-define('API_CLIENT_TOKEN', '');
-define('API_USERID_TOKEN', '');
-
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+$dotenv->load();
