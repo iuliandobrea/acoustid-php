38707255bec7aba76e3edcedd12dc40af6f601f7
c0dex <petrovgm@gmail.com>
2019-08-16 21:02:56 +0300

    Tests

---
+++ tests/AcoustId/RequestTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\AcoustId;
+
+use AcoustId\AcoustId;
+use AcoustId\Request;
+use Tests\TestCase;
+
+/**
+ * Class RequestTest
+ *
+ * @package Tests\AcoustId
+ */
+class RequestTest extends TestCase
+{
+    protected $acoustId;
+    protected $instance;
+
+    /**
+     * RequestTest constructor.
+     *
+     * @param string|null $name
+     * @param array       $data
+     * @param string      $dataName
+     */
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+    }
+
+    /**
+     * @throws \AcoustId\Exceptions\InvalidArgumentException
+     */
+    protected function setUp()
+    {
+        /** @var AcoustId $acoustId */
+        $this->acoustId = new class(getenv('API_APPLICATION_TOKEN')) extends AcoustId
+        {
+        };
+        $this->acoustId->setAPIUrl('https://api.acoustid.org/v2/lookup');
+
+        /** @var Request instance */
+        $this->instance = new class($this->acoustId) extends Request
+        {
+            function composeQueryParameters(): array
+            {
+
+            }
+        };
+    }
+
+    /**
+     *
+     */
+    public function testSetOptions()
+    {
+        $this->instance->setOptions(['test' => 123]);
+        $this->assertEquals(123, $this->instance->getOptions()['test']);
+    }
+}
