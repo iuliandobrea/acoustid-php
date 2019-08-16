49f5dbb353c01f0b78c07913de89f854dd73ee3c
c0dex <petrovgm@gmail.com>
2019-08-17 00:27:59 +0300

    Request list class

---
+++ src/Request/ListByMBId.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AcoustId\Request;
+
+use AcoustId\Request;
+
+/**
+ * Class ListByMBId
+ *
+ * @package AcoustId\Request
+ */
+class ListByMBId extends Request
+{
+    /**
+     * @var \AcoustId\ListByMBId
+     */
+    protected $search;
+
+    /**
+     * ListByMBId constructor.
+     *
+     * @param \AcoustId\ListByMBId $instance
+     */
+    public function __construct(\AcoustId\ListByMBId $instance)
+    {
+        parent::__construct($instance);
+
+        $this->search     = $instance;
+        $classQueryParams = $this->composeQueryParameters();
+
+        $this->createFullQueryString($classQueryParams);
+    }
+
+    /**
+     * @param \AcoustId\ListByMBId $list
+     *
+     * @return array
+     */
+    protected function createBaseQueryString($list): array
+    {
+        $query = parent::createBaseQueryString($list);
+
+        $query['mbid'] = $list->getMBId();
+
+        return $query;
+    }
+
+    /**
+     * @return array
+     */
+    protected function composeQueryParameters(): array
+    {
+        $query = $this->createBaseQueryString($this->search);
+
+        if ($this->search->getResponseType() == 'jsonp') {
+            $query['jsoncallback'] = $this->search->getJsonCallBack();
+        }
+
+        return $query;
+    }
+}
